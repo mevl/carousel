d63b7befa09876d39ced28ba688583fbf1db92c6
Vladimir Melnikov <vladimir@af7.ru>
2017-04-26 10:53:11 +0300

    Refactoring

--- carousel.js
+++ carousel.js
@@ -11,22 +11,29 @@ class Carousel {
       btnPrevSelector: '.arrow.prev',
       btnNextSelector: '.arrow.next',
       containerSelector: '.images',
-      imagesSelector: '.images li',
+      itemsSelector: 'li',
       scrollStep: 3,
     };
     this.options = Object.assign({}, defaultOptions, options);
     this.btnPrev = element.querySelector(this.options.btnPrevSelector);
     this.btnNext = element.querySelector(this.options.btnNextSelector);
     this.container = element.querySelector(this.options.containerSelector);
-    this.images = element.querySelectorAll(this.options.imagesSelector);
     this.scrollPosition = 0;
+    this.positionOffset = [];
+
+    const items = this.container.querySelectorAll(this.options.itemsSelector);
+    Array.prototype.forEach.call(items, (item, i) => {
+      this.positionOffset[i] = i
+        ? this.positionOffset[i - 1] + items[i - 1].getBoundingClientRect().width
+        : 0;
+    });
 
     this.btnPrev.addEventListener('click', this.scroll.bind(this, -1));
     this.btnNext.addEventListener('click', this.scroll.bind(this, 1));
   }
 
   /**
-   * Scrolling the carousel.
+   * Next/Previous scroll button callback.
    * @param {number} direction The direction (1 - next, -1 - previous)
    */
   scroll(direction = 1) {
@@ -37,17 +44,24 @@ class Carousel {
     } else {
       this.btnPrev.classList.remove('hidden');
     }
-    if (this.scrollPosition >= this.images.length - this.options.scrollStep) {
-      this.scrollPosition = this.images.length - this.options.scrollStep;
+    if (this.scrollPosition >= this.positionOffset.length - this.options.scrollStep) {
+      this.scrollPosition = this.positionOffset.length - this.options.scrollStep;
       this.btnNext.classList.add('hidden');
     } else {
       this.btnNext.classList.remove('hidden');
     }
 
-    let offset = 0;
-    for (let i = 0; i < this.scrollPosition; i += 1) {
-      offset += this.images[i].getBoundingClientRect().width;
+    this.scrollTo(this.container, this.scrollPosition);
+  }
+
+  /**
+   * Scrolling the carousel to item position.
+   * @param {HTMLElement} element The container HTML element.
+   * @param {number} position The destination scroll position.
+   */
+  scrollTo(element, position) {
+    if (element && this.positionOffset[position] !== undefined) {
+      element.style.marginLeft = `${this.positionOffset[position] * -1}px`;
     }
-    this.container.style.marginLeft = `${offset * -1}px`;
   }
 }
